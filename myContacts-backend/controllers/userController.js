24771cf303a12fe902c6ae61cead7c7d5f57bbce
PriyankaChouhan1019 <priyachouhan1019@gmail.com>
2024-03-27 16:10:24 +0530

    generate jwt token and login user

--- myContacts-backend/controllers/userController.js
+++ myContacts-backend/controllers/userController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
 const User = require("../models/userModels")
-
+const jwt = require("jsonwebtoken");
 
 //@desc Register the user
 //@route POST /api/users/register
@@ -42,7 +42,31 @@ const registerUser = asyncHandler(async (req,res)=>{
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req,res)=>{
-    res.json({message: "login the user"})
+    const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  const user = await User.findOne({ email });
+  
+  //compare password with hashedpassword
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECERT,
+      { expiresIn: "15m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("email or password is not valid");
+  }
 });
 
 //@desc current  user
